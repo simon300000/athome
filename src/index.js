2b58707b7f08e4f5254755cbbf7c5e42a7f2f7b4
simon3000 <simon3000@163.com>
2019-05-24 05:37:52 +0800

    Inbound-Outbound

--- src/index.js
+++ src/index.js
@@ -1,57 +1,100 @@
 const uuid = require('uuid/v4')
+const { once } = require('events')
+
+const Client = require('socket.io-client')
 const Server = require('socket.io')
 
 class AtHome {
-  constructor({ id = uuid() } = {}) {
+  constructor({ id = uuid(), log = () => {} } = {}) {
     this.id = id
     this.ability = {}
-    this.upstream = {}
-    this.downstream = {}
+    // this.upstream = {}
+    // this.downstream = {}
+    this.ports = {}
     this.inbounds = {}
     this.outbounds = {}
+    this.log = log
+    this.log('Initialized', this.id)
   }
-  connect({ port, type }) {
+  connect({ target, protocol = 'ws', id = uuid() }) {
+    let inbound = new Inbound({ target, protocol, id, log: this.log }, {})
+    this.inbounds[id] = inbound
+    this.log('Inbound Initialized', protocol, target, id)
+  }
+  // updateBusy(name) {
+  //   let busy = Object.keys(this.ability[name].outbounds)
+  //     .map(id => this.ability[name].outbounds[id].busy)
+  //     .reduce((a, b) => a && b)
+  //   if (this.ability[name].busy !== busy) {
+  //     this.ability[name].busy = busy
+  //   }
+  // }
+  open({ target, protocol = 'ws', id = uuid() }) {
+    let port = new Port({ target, protocol, id, log: this.log }, this)
+    this.ports[id] = port
+    this.log('Port Open', protocol, target, id)
+  }
+}
 
+class Inbound {
+  constructor({ id, protocol, target, log }) {
+    this.id = id
+    this.protocol = protocol
+    this.target = target
+    this.online = false
+    this.log = log
+    if (protocol === 'ws') {
+      let socket = Client(target)
+      this.socket = socket
+      socket.on('connect', () => {
+        log('Inbound Connect', id)
+        this.login()
+      })
+    }
   }
-  checkBusy(name) {
-    return Object.keys(this.ability[name].outbounds)
-      .map(id => this.ability[name].outbounds[id].busy)
-      .reduce((a, b) => a && b)
+  emit(event, data) {
+    return new Promise(resolve => this.socket.emit(event, data, resolve))
   }
-  open({ port, protocol = 'processer', id = uuid() }) {
-    let outbound = new Outbound({ port, protocol, id }, {
-      register: name => {
-        if (!this.ability[name]) {
-          this.ability[name] = { outbounds: {}, busy: false }
-        }
-        this.ability[name].outbounds[id] = { busy: false }
-      },
-      busy: name => {
-        this.ability[name].outbounds[id].busy = true
-        this.ability[name].busy = this.checkBusy(name)
-      },
-      free: name => {
-        this.ability[name].outbounds[id].busy = false
-        this.ability[name].busy = this.checkBusy(name)
-      }
-    })
-    this.outbounds[outbound.id] = outbound
+  async login() {
+    let message = await this.emit('authenticate', { id: this.id })
+    if (message === 'welcome') {
+      this.online = true
+      this.log('Inbound Online', this.id)
+    }
+  }
+}
+
+class Port {
+  constructor({ target, protocol, id, log }, node) {
+    this.id = id
+    this.ability = {}
+    this.target = target
+    this.node = node
+    this.protocol = protocol
+    this.log = log
+    if (protocol === 'ws') {
+      let io = new Server(target, { serveClient: false })
+      this.io = io
+      io.on('connect', socket => this.authenticate(socket))
+    }
+  }
+  async authenticate(socket) {
+    let [authenticate, arc] = await once(socket, 'authenticate')
+    if (typeof arc !== 'function') {
+      arc = () => {}
+    }
+    let { id } = authenticate
+    let outbound = new Outbound({ protocol: this.protocol, socket })
+    this.node.outbounds[id] = outbound
+    arc('welcome')
+    this.log('Outbound Online', id, 'from', this.id)
   }
 }
 
 class Outbound {
-  constructor({ port, protocol, id }, { register, busy, free }) {
-    this.id = id
-    this.ability = {}
-    this.port = port
-    this.register = register
-    this.busy = busy
-    this.free = free
+  constructor({ protocol, socket }) {
     this.protocol = protocol
-    if (protocol === 'ws') {
-      const io = new Server(port, { serveClient: false })
-      this.io = io
-    }
+    this.socket = socket
   }
 }
 
